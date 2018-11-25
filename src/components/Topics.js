70a9dbfe9549ba402b0efef5018148fe1abb3a6b
dwyfrequency <jdwy215@gmail.com>
2018-11-24 21:52:21 -0500

    adding new component

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { Link, Route } from "react-router-dom";
+
+// stop time: 1:08:58
+function Topic(props) {
+  return <div>Topic</div>;
+}
 
 export default function Topics(props) {
-  return <div>Topics</div>;
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to="/topics/rendering">Rendering with React</Link>
+        </li>
+        <li>
+          <Link to="/topics/components">Components</Link>
+        </li>
+        <li>
+          <Link to="/topics/props-v-state">Props vs. State</Link>
+        </li>
+      </ul>
+
+      <hr />
+      <Route path="/topics/rendering" component={Topic} />
+    </div>
+  );
 }
