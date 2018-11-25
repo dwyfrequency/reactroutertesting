9805d8dcc3cb2afd7721ac0077e4af3aa55422b0
dwyfrequency <jdwy215@gmail.com>
2018-11-25 08:31:01 -0500

    adding nested routes

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -3,28 +3,33 @@ import { Link, Route } from "react-router-dom";
 
 // stop time:
 function Topic({ match }) {
+  // match is a prop that react router will pass to any component it renders
+  // we are getting the url parameter that we established with the route component
   return <div>{match.params.topicId}</div>;
 }
 
-export default function Topics(props) {
+export default function Topics({ match }) {
+  // destructuring match from props
+  // when you are building nested link components use match.url
   return (
     <div>
       <h2>Topics</h2>
       <ul>
         <li>
-          {/* to says where to link to when clicked */}
-          <Link to="/topics/rendering">Rendering with React</Link>
+          {/* the 'to' keyword says where to link to when clicked */}
+          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
         </li>
         <li>
-          <Link to="/topics/components">Components</Link>
+          <Link to={`${match.url}/components`}>Components</Link>
         </li>
         <li>
-          <Link to="/topics/props-v-state">Props vs. State</Link>
+          <Link to={`${match.url}/props-v-state`}>Props vs. State</Link>
         </li>
       </ul>
-
       <hr />
-      <Route path="/topics/:topicId" component={Topic} />
+      {/* This is dynamic rendering. We are also using nested routes below. Routes are just components */}
+      {/* when you are building nested route components use match.path - (difference between url, the path allows us to use dynamic routes)  */}
+      <Route path={`${match.path}/:topicId`} component={Topic} />
     </div>
   );
 }
